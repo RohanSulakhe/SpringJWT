43a98b02cb79821ec45a8aba256dd7ad1eb909b8
RohanSulakhe <rohanusulakhe@gmail.com>
2025-11-29 12:59:06 +0530

    added method: getcsrf

--- src/main/java/com/rohan/springauth/studentController.java
+++ src/main/java/com/rohan/springauth/studentController.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class studentController {
@@ -16,6 +19,11 @@ public class studentController {
         new student(1,"Rohan", "java"),
         new student(2,"johan", "c++")
     ));
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getcsrf(HttpServletRequest req){
+        return (CsrfToken) req.getAttribute("_csrf");
+    }
     @GetMapping("/studentinfo")
     public List<student> getStud(){
         return students;
