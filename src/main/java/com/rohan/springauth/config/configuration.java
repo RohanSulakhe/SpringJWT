9740e47fdc484b0a9c537890493253afc7a532b5
RohanSulakhe <rohanusulakhe@gmail.com>
2025-12-01 22:27:15 +0530

    demo of without lamba exp

--- src/main/java/com/rohan/springauth/config/configuration.java
+++ src/main/java/com/rohan/springauth/config/configuration.java
@@ -1,16 +1,12 @@
 package com.rohan.springauth.config;
-
-import com.rohan.springauth.SpringauthApplication;
-import com.rohan.springauth.greetController;
-import com.rohan.springauth.studentController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
@@ -20,11 +16,20 @@ public class configuration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         
-        http.csrf(c-> c.disable());
-        http.authorizeHttpRequests(c-> c.anyRequest().authenticated());
-        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        // http.csrf(c-> c.disable());
+        // http.authorizeHttpRequests(c-> c.anyRequest().authenticated());
+        // http.formLogin(Customizer.withDefaults());
+        // http.httpBasic(Customizer.withDefaults());
+        // http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        // Writing above without lamda expression
+        Customizer<CsrfConfigurer<HttpSecurity>> customizer = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(CsrfConfigurer<HttpSecurity> t) {
+                t.disable();
+            }
+        };
 
         return http.build();
     }
