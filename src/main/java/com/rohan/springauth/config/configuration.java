ceee93915a286f36a1e5cfb8baae7091b49412b2
RohanSulakhe <rohanusulakhe@gmail.com>
2025-12-01 22:18:31 +0530

    config changes

--- src/main/java/com/rohan/springauth/config/configuration.java
+++ src/main/java/com/rohan/springauth/config/configuration.java
@@ -1,16 +1,31 @@
 package com.rohan.springauth.config;
 
+import com.rohan.springauth.SpringauthApplication;
+import com.rohan.springauth.greetController;
+import com.rohan.springauth.studentController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
 public class configuration {
+
+    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        
+        http.csrf(c-> c.disable());
+        http.authorizeHttpRequests(c-> c.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         return http.build();
     }
 
