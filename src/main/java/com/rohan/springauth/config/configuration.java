d0f6cdeed82f783f8269a2fa08b889b0031d3502
RohanSulakhe <rohanusulakhe@gmail.com>
2025-12-01 22:30:17 +0530

    ...

--- src/main/java/com/rohan/springauth/config/configuration.java
+++ src/main/java/com/rohan/springauth/config/configuration.java
@@ -16,20 +16,20 @@ public class configuration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         
-        // http.csrf(c-> c.disable());
-        // http.authorizeHttpRequests(c-> c.anyRequest().authenticated());
+        http.csrf(c-> c.disable());
+        http.authorizeHttpRequests(c-> c.anyRequest().authenticated());
         // http.formLogin(Customizer.withDefaults());
-        // http.httpBasic(Customizer.withDefaults());
-        // http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(Customizer.withDefaults());
+        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
 
         // Writing above without lamda expression
-        Customizer<CsrfConfigurer<HttpSecurity>> customizer = new Customizer<CsrfConfigurer<HttpSecurity>>() {
-            @Override
-            public void customize(CsrfConfigurer<HttpSecurity> t) {
-                t.disable();
-            }
-        };
+        // Customizer<CsrfConfigurer<HttpSecurity>> customizer = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+        //     @Override
+        //     public void customize(CsrfConfigurer<HttpSecurity> t) {
+        //         t.disable();
+        //     }
+        // };
 
         return http.build();
     }
